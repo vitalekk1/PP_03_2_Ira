22935cd1e1406c2c0440aa2dc5a0eb3a41f23bd2
vitalekk1 <vitalikkakanov55@gmail.com>
2020-04-26 20:49:16 +0500

    Добавьте файлы проекта.

---
+++ PP_03_2/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PP_03_2
+{
+    class Program
+    {
+        struct startAndEnd{
+            public int start;
+            public int end;
+        }
+        static List<int> listNumber;
+        private static int countThread = 3;
+        private static int h;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите N:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            listNumber = new List<int>();
+            devideCountN(2, n, countThread);
+            int endT = 2 + h;
+            Thread[] threads = new Thread[countThread];
+            int countTh = 0;
+
+            for (int i = 2; i < n; i += h)
+            {
+                startAndEnd struct3 = new startAndEnd();
+                lock (listNumber)
+                {
+                    endT = i + h;
+                    if (endT > n) { endT = n; }
+                    struct3.start = i;
+                    struct3.end = endT;
+                    threads[countTh] = new Thread(check);
+                    threads[countTh].Start(struct3);
+                    countTh++;
+                }
+            }
+
+            foreach(Thread thread in threads)
+            {
+                thread.Join();
+            }
+
+            listNumber = listNumber.Distinct().ToList();
+
+            foreach (int i in listNumber)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+
+        }
+
+        private static void check(object struc)
+        {
+            startAndEnd struct1 = (startAndEnd)struc;
+            int start = struct1.start;
+            int end = struct1.end;
+            int num; int temp;
+            for (int i = start; i <= end; ++i)
+            {
+                temp = i; num = 0;
+                while (temp != 0)
+                {
+                    num = num * 10 + (temp % 10);
+                    temp /= 10;
+                }
+                if (num == i) listNumber.Add(i);
+            }
+        }
+
+        private static void devideCountN(double start, double end, int countN)
+        {
+            h =(int) Math.Round((end - start) / countN);
+        }
+
+
+    }
+}
